959caa70fb554d239a430f8d5fc4ffdaa921fe57
thenry0401 <thenry0401@gmail.com>
2017-05-29 16:49:16 +0900

    Dynamic Post Query in post_list view

--- django_app/blog/models.py
+++ django_app/blog/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    objects = None
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,7 +1,19 @@
+from django.utils import timezone
+
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .models import Post
 
 
 def post_list(request):
     # return HttpResponse('<html><body>Post List</body></html>')
-    return render(request, 'blog/post_list.html')
+    # posts변수에 ORM을 이용해서 전체 Post의 리스트(쿼리셋)을 대입
+    # posts = Post.objects.all()
+    # print(posts)
+    posts = Post.objects.filter(published_date__lte=timezone.now())
+    context ={
+        'title' : 'PostList from post-list view',
+        'posts' : posts,
+    }
+    return render(request, 'blog/post_list.html', context=context)
